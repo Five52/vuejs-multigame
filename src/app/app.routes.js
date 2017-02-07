b2677612d93d2813424b89d0ac0860f9afaf872c
Five52 <five52@laposte.net>
2017-02-07 16:38:48 +0100

    Add component Stats

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -7,12 +7,14 @@ Vue.use(VueRouter);
 import Categories from './components/Categories.vue';
 import Practice from './components/Practice.vue';
 import Tables from './components/Tables.vue';
+import Stats from './components/Stats.vue';
 
 export const router = new VueRouter({
   mode: 'history',
   routes: [
     {path: '/', name: 'home', component: Categories},
     {path: '/tables', name: 'tables', component: Tables},
-    {path: '/apprendre/:id', name: 'practice', component: Practice, props: true}
+    {path: '/apprendre/:id', name: 'practice', component: Practice, props: true},
+    {path: '/stats', name: 'stats', component: Stats}
   ]
 });
---
+++ src/app/components/Stats.vue
@@ -0,0 +1,42 @@
+<template>
+  <table>
+    <tr>
+      <td></td>
+      <td v-for="table in maxTable">{{ table }}</td>
+    </tr>
+    <tr v-for="table in maxTable">
+      <td>{{ table }}</td>
+      <td v-for="table in maxTable"></td>
+    </tr>
+  </table>
+</template>
+
+<script>
+  import GlobalStore from '../store/GlobalStore';
+  import {maxTable} from '../store/CalculusStore';
+
+  export default {
+    name: 'Stats',
+    data() {
+      return {
+        maxTable
+      }
+    },
+    created() {
+      GlobalStore.title = 'Statistiques'
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "../../main.scss";
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 20px;
+    height: 20px;
+    text-align: center;
+  }
+</style>
